46dcd4faf634ab4c97d0189a56bb77d2df39d7f8
xxristoskk <katsaros.christopher@gmail.com>
2021-04-08 12:23:57 -0400

    moving this project to new repo

--- xristosk/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='blog'),
-    path('<int:pk>/', views.PostDetail.as_view(), name='blog_detail'),
-    path('post/', views.AddBlog.as_view(), name='add_blog')
-]
--- xristosk/frontpage/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(label='name', max_length=100)
-    subject = forms.CharField(label='subject', max_length=200)
-    sender = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
