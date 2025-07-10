f5f728490e764363dda631b76b8ab483c8a048d0
xxristoskk <katsaros.christopher@gmail.com>
2025-07-09 17:15:09 -0700

    Update Vite config to use relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Update base URL for user GitHub Pages site
+  base: './', // Use relative paths for assets
 })
